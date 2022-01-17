79bd0e8246308cd114a77a72ce46d9efe7033954
Chris Lee <minjaelee0727@gmail.com>
2022-01-17 00:35:17 -0500

    rebase

--- Shared/Domain/Constants/Colors.swift
+++ Shared/Domain/Constants/Colors.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 let backgroundColor: UIColor = UIColor(red: 0.9682741117, green: 0.9682741117, blue: 0.9682741117, alpha: 1)
+let backgroundDarkMode: UIColor =  UIColor(hexString: "#121212")
 let pastelGreen: UIColor = UIColor(hexString: "#77DD77")
